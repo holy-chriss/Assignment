9d6f1c6557cb26c2184d8e241e145f06811286d8
holy-chriss <125399672+holy-chriss@users.noreply.github.com>
2023-02-14 22:22:59 +0300

    Add files via upload

---
+++ Qustion 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	float celcius,farenheit;
+	celcius=27;
+	farenheit=(celcius+32)*1.8;
+	printf("the temp in farenheit is:%f",farenheit);
+	return 0;
+}
