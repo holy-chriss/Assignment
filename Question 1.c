c61817d6995f44cdf39bac807a5be4a80c8cf517
holy-chriss <125399672+holy-chriss@users.noreply.github.com>
2023-02-14 22:01:43 +0300

    Add files via upload

---
+++ Question 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void)
+{
+	int sum;
+	sum=25+37-19;
+	printf("the answer is %i",sum);
+	return 0;
+}
