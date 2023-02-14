e6adc584d5d0f21296ca776754d744842febb553
holy-chriss <125399672+holy-chriss@users.noreply.github.com>
2023-02-14 22:25:16 +0300

    Add files via upload

---
+++ Question 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	int x=6;
+	int y=4;
+	float divide= (float) 6/4;
+	printf("%f",divide);
+	return 0;
+}
